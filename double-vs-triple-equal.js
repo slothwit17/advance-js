ed69b369cbe0ee1bd12aac989201bedc0f8b9679
slothwit17 <rakibuzzamansiam@gmail.com>
2022-09-16 01:09:41 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+const first = 2;
+const second = '2';
+if (first == second) {
+    console.log('condition is true');
+} else{
+    console.log('condition is false');
+}
+
+const first1 = 2;
+const second2 = '2';
+if (first1 === second2) {
+    console.log('condition is true');
+} else{
+    console.log('condition is false');
+}
+
+// in triple equal it will check both value and type but in double equal it will try to convert the type with some step and then it will compare the value.
