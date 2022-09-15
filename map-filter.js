62d8de00c22c8aad93643d3bc3a3238cdc59c509
slothwit17 <rakibuzzamansiam@gmail.com>
2022-09-16 01:37:56 +0600

    map filter find

--- map-filter.js
+++ map-filter.js
@@ -1,15 +1,31 @@
 const numbers = [3, 6, 9, 19, 21, 8];
-// const output =[];
-// for (let i = 0; i < numbers.length; i++) {
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
+const output =[];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
 
-// console.log(output);
+console.log(output);
 
 function square(element){
     return element * element;
 }
 const result2 = numbers.map(square);
 console.log(result2);
+
+
+
+numbers.map(function(element, index, array){
+    console.log(element, index, array);
+});
+
+
+const result3 = numbers.map(x => x*x);
+console.log(result3);
+
+const bigger = numbers.filter( x => x > 10); // filter return an array
+console.log(bigger);
+
+const isThere = numbers.find( x => x < 10); // find return just an element
+console.log(isThere);
